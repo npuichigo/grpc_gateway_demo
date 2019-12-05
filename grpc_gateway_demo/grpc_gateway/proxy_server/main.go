4c4230f2d42e206874944332f522051101158631
npuichigo <418121364@qq.com>
2019-12-05 05:35:26 +0000

    Refactor grpc gateway to be more user-friendly

--- grpc_gateway_demo/grpc_gateway/proxy_server/main.go
+++ grpc_gateway_demo/grpc_gateway/proxy_server/main.go
@@ -1,55 +1,35 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"net/http"
 
 	"github.com/golang/glog"
-	"golang.org/x/net/context"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"google.golang.org/grpc"
-
-	gw "github.com/npuichigo/grpc_gateway_demo/grpc_gateway/service_gw"
+	"github.com/npuichigo/grpc_gateway_demo/grpc_gateway/gateway"
 )
 
 var (
-	proxyEndpoint = flag.String("proxy_endpoint", "localhost:9090", "endpoint of DemoService")
+		endpoint   = flag.String("endpoint", "localhost:9090", "endpoint of the gRPC service")
+		network    = flag.String("network", "tcp", `one of "tcp" or "unix". Must be consistent to -endpoint`)
 )
 
-func run() error {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	mux := runtime.NewServeMux()
-	runtime.SetHTTPBodyMarshaler(mux)
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := gw.RegisterDemoHandlerFromEndpoint(ctx, mux, *proxyEndpoint, opts)
-	if err != nil {
-	  return err
-	}
-
-	s := &http.Server {
-	  Addr: ":8080",
-	  Handler: allowCORS(mux),
-	}
-
-	glog.Infof("Starting listening at %s", ":8080")
-	if err := s.ListenAndServe(); err != http.ErrServerClosed {
-	  glog.Errorf("Failed to listen and server: %v", err)
-	  return err
-	}
-	return nil
+func init() {
+	flag.Set("logtostderr", "true")
 }
 
 func main() {
 	flag.Parse()
 	defer glog.Flush()
 
-	glog.Info("Running grpc gateway proxy server on 0.0.0.0:8080")
-	glog.Info("Grpc endpoint is: ", proxyEndpoint)
-
-	if err := run(); err != nil {
-	  glog.Fatal(err)
+	ctx := context.Background()
+	opts := gateway.Options{
+		Addr: ":8080",
+		GRPCServer: gateway.Endpoint{
+			Network: *network,
+			Addr:    *endpoint,
+		},
+	}
+	if err := gateway.Run(ctx, opts); err != nil {
+		glog.Fatal(err)
 	}
 }
